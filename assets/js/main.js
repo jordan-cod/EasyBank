a2a695f3ce83d041d1ca77c132fc37dbb89d22c6
Gabriel Jordan <gabrijordan3@gmail.com>
2021-07-04 00:33:55 -0300

    add new lines to open/close the menu with a gray background

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,4 +1,19 @@
+let MenuOpen = false
+
 function toggleAnimation() {
     const menuBtn = document.querySelector('.menu-bars')
-    menuBtn.classList.toggle('open')
+    const Menu = document.querySelector('.menu-nav')
+    const backgroundMenu = document.querySelector('.fundo_menu')
+    
+    if(MenuOpen === false){
+        menuBtn.classList.add('open')
+        Menu.classList.add('isOpen')
+        backgroundMenu.classList.add('menu-ativo')
+        MenuOpen = true
+    } else{
+        menuBtn.classList.remove('open')
+        Menu.classList.remove('isOpen')
+        backgroundMenu.classList.remove('menu-ativo')
+        MenuOpen = false
+    }
 }
